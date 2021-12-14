d3ae8066cbda69a5f0ebe6ca80c042bcf9f67957
gian251 <samontegian251@yahoo.com>
2021-12-15 01:01:49 +0800

    update 101

--- src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
@@ -13,7 +13,7 @@ import java.util.*;
 @CrossOrigin("*")
 public class EmployeeController {
     
-    @Autowired 
+    @Autowired
     private EmployeeRepository employeeRepository;
     
     @GetMapping("/employees")
--- src/main/java/com/csa/samplefullstack/controllers/HelloController.java
+++ src/main/java/com/csa/samplefullstack/controllers/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello spring!";
+        return "hello spring";
     }
 }
